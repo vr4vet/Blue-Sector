b82523e668cb5858915fc337a86bc265d1d4979d
Emil Aron Andresen Mathiesen <eamathie@stud.ntnu.no>
2023-03-15 11:32:56 +0100

    feat: Added fish model, visualisation of system, fixed food particles
    optional body: Changed some variables to make them clearer, and the fish
    system is now more customisable via the editor insted of having to
    change the code

--- Assets/FishFeeding/Fish/FishScript.cs
+++ Assets/FishFeeding/Fish/FishScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class FishScript : MonoBehaviour
@@ -19,44 +20,43 @@ public class FishScript : MonoBehaviour
     private float swimSpeedVertical;
     private float swimSpeedHorizontal;
 
+    private Animation fishAnimation;
     private GameObject fishSystem;
     private FishSystemScript fishSystemScript;
-
     
     // Start is called before the first frame update
     void Start()
-    {
-        
-        InvokeRepeating(nameof(periodicUpdates), Random.Range(0.0f, 5.0f), 3.0f);
-        //fishSystem = GameObject.FindGameObjectWithTag("Fish System");
+    {   
+        InvokeRepeating(nameof(PeriodicUpdates), Random.Range(0.0f, 5.0f), 3.0f);
         fishSystem = GameObject.Find("FishSystem");
         fishSystemScript = fishSystem.GetComponent<FishSystemScript>();
+        fishAnimation = gameObject.transform.GetChild(0).GetComponent<Animation>();
+        
         radius = fishSystemScript.radius;
         fullness = 100.0f; //Random.Range(0.0f, 100.0f);
-        top = (fishSystem.transform.position.y + (fishSystemScript.height / 2)) - 1;  // top of merd/water surface
-        bottom = (fishSystem.transform.position.y - (fishSystemScript.height / 2)) + 1; // bottom of merd
+        top = (fishSystem.transform.position.y + (fishSystemScript.height / 2)) - 1.5f;  // top of merd/water surface
+        bottom = (fishSystem.transform.position.y - (fishSystemScript.height / 2)) + 1.5f; // bottom of merd
         fullnessDivider = bottom + (((bottom - top) * (fishSystemScript.fullnessDivider)) * -1); // border between hungry and full fish
         hungerRate = fishSystemScript.hungerRate;
         swimSpeedVertical = fishSystemScript.swimSpeedVertical;
         swimSpeedHorizontal = fishSystemScript.swimSpeedHorizontal;
         fullnessLimit = fishSystemScript.fullnessLimit;
         dead = false;
-
-        //Debug.Log(fishSystemScript.height);
-        //Debug.Log(top);
-        Debug.Log(fishSystemScript.swimSpeedVertical);
-
     }
 
     private bool waitingForReturn = false;
     // Update is called once per frame
     void Update()
     {
+        if (dead)
+        {
+            fishAnimation.Stop();
+        }
+
         if (IsColliding())
         {
             if (!waitingForReturn)
             {
-                //movement = new Vector3(0 - gameObject.transform.position.x, 0 - gameObject.transform.position.y, 0 - gameObject.transform.position.z);
                 movement.x *= -1;
                 movement.z *= -1;
                 Quaternion target = Quaternion.LookRotation(movement);
@@ -85,7 +85,7 @@ public class FishScript : MonoBehaviour
     }
 
     // runs every 3rd second
-    void periodicUpdates()
+    void PeriodicUpdates()
     {
         float posY = gameObject.transform.position.y;
 
@@ -117,9 +117,6 @@ public class FishScript : MonoBehaviour
             dead = true;
         }
 
-        //Debug.Log(fullness);
-        //Debug.Log(posY);
-
         if (dead)
         {
             if (posY > bottom)
@@ -149,7 +146,6 @@ public class FishScript : MonoBehaviour
             // Rotates the fish in the right direction
             randX = Random.Range(-swimSpeedHorizontal, swimSpeedHorizontal);
             randZ = Random.Range(-swimSpeedHorizontal, swimSpeedHorizontal);
-            //randY = Random.Range(-0.3f, 0.3f);
 
             movement = new Vector3(randX, randY, randZ);
             Quaternion target = Quaternion.LookRotation(movement);
---
+++ Assets/FishFeeding/FishSystem/FishSystemScript.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FishSystemScript : MonoBehaviour
+{
+    public GameObject fish;
+    private ParticleSystem foodParticles;
+    public float radius = 10;
+    public float height = 20;
+    public float amountOfFish = 5;
+    public float fullnessDivider = 0.7f;
+    public float fullnessLimit = 70;
+    public float hungerRate = 3.0f;
+    public float swimSpeedVertical = 0.2f;
+    public float swimSpeedHorizontal = 0.1f;
+    public bool feeding = false;    // all fish in the top part ("hunger zone") will be fed when this is true
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foodParticles = gameObject.GetComponent<ParticleSystem>();
+        // positioning particles at top of fish system
+        for (int i = 0; i < amountOfFish; i++)
+        {
+            Instantiate(fish, new Vector3(Random.Range(-radius + 3, radius - 3), Random.Range(-height/2 + 3, height/2- 3), Random.Range(-radius + 3, radius - 3)), fish.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            feeding = true;
+            foodParticles.Play();
+        } else
+        {
+            feeding = false;
+            foodParticles.Stop();
+        }
+    }
+}
+
+[CustomEditor(typeof(FishSystemScript))]
+public class FishSystemVisualization : Editor
+{
+    public void OnSceneGUI()
+    {
+        // getting parameters for drawing visualization
+        var t = target as FishSystemScript;
+        var transform = t.transform;
+        var position = t.transform.position;
+        float height = t.height;
+        float radius = t.radius;
+
+        // drawing top and bottom of system
+        float top = t.transform.position.y + (height / 2);  // top of merd/water surface
+        float bottom = t.transform.position.y - (height / 2); // bottom of merd
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(position + new Vector3(0, top, 0), transform.up, radius);
+        Handles.DrawWireDisc(position + new Vector3(0, bottom, 0), transform.up, radius);
+        
+        // drawing vertical lines form top to bottom
+        Handles.DrawLine(position + new Vector3(radius, top, 0), position + new Vector3(radius, bottom, 0));
+        Handles.DrawLine(position + new Vector3(-radius, top, 0), position + new Vector3(-radius, bottom, 0));
+        Handles.DrawLine(position + new Vector3(0, top, radius), position + new Vector3(0, bottom, radius));
+        Handles.DrawLine(position + new Vector3(0, top, -radius), position + new Vector3(0, bottom, -radius));
+
+        // drawing the divider between the zones of hungry and full fish
+        float fullnessDivider = bottom + (((bottom - top) * (t.fullnessDivider)) * -1); // border between hungry and full fish
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(position + new Vector3(0, fullnessDivider, 0), transform.up, radius);
+
+        // moving food particle system to the top of fish system
+        var foorParticlesShape = GameObject.Find("FishSystem").GetComponent<ParticleSystem>().shape;
+        foorParticlesShape.position = new Vector3(foorParticlesShape.position.x, GameObject.Find("FishSystem").transform.position.y + (height / 2), foorParticlesShape.position.z);
+
+    }
+
+}
