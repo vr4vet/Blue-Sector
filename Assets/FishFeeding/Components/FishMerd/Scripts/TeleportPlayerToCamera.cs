22f31a73373ca10b2a3b08f77ad0220dfcb3557d
Trym Lund Flogard <trym@flogard.no>
2023-03-16 11:30:12 +0100

    tmp

---
+++ Assets/FishFeeding/Components/FishMerd/Scripts/TeleportPlayerToCamera.cs
@@ -0,0 +1,30 @@
+﻿using BNG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Valve.VR;
+
+public class TeleportPlayerToCamera : MonoBehaviour {
+    [SerializeField]
+    public BNGPlayerController Player;
+
+    //public Valve.VR.InteractionSystem.Player player;
+    private Vector3 playerStartPosition;
+    private Quaternion playerStartRotation;
+
+    //moves player to the camera position
+    public void TeleportToCamera() {
+        if (gameObject.GetComponent<Camera>().enabled == true) {
+            //save player position for the return trip
+            //playerStartPosition = player.transform.position;
+            //playerStartRotation = player.transform.rotation;
+            Debug.Log(playerStartPosition);
+            //player.transform.SetPositionAndRotation(gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+
+    // Moves player to original position
+    public void TeleportBack() {
+        //player.transform.SetPositionAndRotation(playerStartPosition, playerStartRotation);
+    }
+}
