6019ab6feef2c9a0d33e38d2d40200e10959bdfd
MagnusBau <magnus.baugerd@hotmail.com>
2023-04-13 17:37:16 +0200

    feat: working on fish movement

--- Assets/FishWelfare/Scripts/Fish.cs
+++ Assets/FishWelfare/Scripts/Fish.cs
@@ -10,6 +10,13 @@ public class Fish : MonoBehaviour
     private int gillDamageGuessed = 0;
     [SerializeField]
     private int id;
+    
+    //The following variables are used for handeling behaviour with water
+    private Vector3 waterBodyCenter;
+    private Vector3 targetPosition;
+    private float waterBodyXLength;
+    private float waterBodyZLength;
+    private bool isInWater;
 
     InspectionTaskManager inspectionTaskManager;
 
@@ -17,17 +24,46 @@ public class Fish : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Skamløst kokt fra FishScript.cs:
+        InvokeRepeating(nameof(PeriodicUpdates), Random.Range(0.0f, 5.0f), 3.0f);
         inspectionTaskManager = GameObject.FindObjectOfType<InspectionTaskManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isInWater){
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, 2 * Time.deltaTime);
+        }
+    }
+
+    void PeriodicUpdates() {
+        SetMoveTarget();
+    }
+
+    private void SetMoveTarget() {
+        //Mer Skamløs koking fra FishScript.cs:
+        Debug.Log("Setting movement");
+        float randX = Random.Range(waterBodyCenter.x -waterBodyXLength / 2,waterBodyCenter.x + waterBodyXLength / 2);
+        float randZ = Random.Range(waterBodyCenter.z -waterBodyZLength / 2,waterBodyCenter.z + waterBodyZLength / 2);
+        targetPosition = new Vector3(randX, transform.position.y, randZ);
+        Debug.Log("xLength: " + randX + " zLength " + randZ);
+        Quaternion target = Quaternion.LookRotation(targetPosition);
+        target = Quaternion.RotateTowards(gameObject.transform.rotation, target, 360);
+        gameObject.transform.rotation = target;
+    }
+
+    public void UpdateWaterBody(float waterheight, Vector3 bodyCenter, float xLength, float zLength, bool isInWater){
+        Debug.Log("Updating body");
+        gameObject.GetComponent<Floating>().waterHeight = waterheight;
+        waterBodyCenter = bodyCenter;
+        waterBodyXLength = xLength;
+        waterBodyZLength = zLength;
+        this.isInWater = isInWater;
     }
 
     public void SetAsSelectedFish() {
-        inspectionTaskManager.SetSelectedFish(this);
+        inspectionTaskManager.SetSelectedFish(this); 
     }
 
     public void SetgillDamageGuessed(int guess) {
--- Assets/FishWelfare/Scripts/TankController.cs
+++ Assets/FishWelfare/Scripts/TankController.cs
@@ -6,10 +6,12 @@ public class TankController : MonoBehaviour
 {
     private InspectionTaskManager inspectionTaskManager;
     public bool isGoal = false;
+    private BoxCollider collider = new BoxCollider();
     // Start is called before the first frame update
     void Start()
     {
         inspectionTaskManager = GameObject.FindObjectOfType<InspectionTaskManager>();
+        collider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -24,8 +26,8 @@ public class TankController : MonoBehaviour
         if(isGoal){
             inspectionTaskManager.ProgressInspection(other.gameObject);
         }
-        if(other.tag == "Floatable"){
-            other.gameObject.GetComponent<Floating>().waterHeight = other.transform.localScale.y + other.transform.position.y;
+        if(other.tag == "Fish"){
+            other.gameObject.GetComponent<Fish>().UpdateWaterBody(other.transform.localScale.y + other.transform.position.y, transform.position, transform.localScale.x, transform.localScale.z, true);
         }
     }
 
@@ -33,8 +35,8 @@ public class TankController : MonoBehaviour
         if(isGoal){
             inspectionTaskManager.RegressInspection(other.gameObject);
         }
-        if(other.tag == "Floatable"){
-            other.GetComponent<Floating>().waterHeight = 0f;
+        if(other.tag == "Fish"){
+            other.gameObject.GetComponent<Fish>().UpdateWaterBody(0f, new Vector3(0f,0f,0f), 0f, 0f, false);
         }
     }
 }
