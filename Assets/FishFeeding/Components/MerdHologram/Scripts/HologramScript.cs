49af04c9ce813dff8a51ee7fea4001b0839544ec
Emil Aron Andresen Mathiesen <eamathie@stud.ntnu.no>
2023-06-11 12:31:14 +0200

    feat: 3D model of hologram and logic for camera. Refs: #131 and #136

---
+++ Assets/FishFeeding/Components/MerdHologram/Scripts/HologramScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramScript : MonoBehaviour
+{
+    private GameObject hologramCamera;
+    private Camera currentCamera;
+    private float holoZ;
+    private float holoY;
+    private FishSystemScript currentFishSystem;
+    private Camera[] cameras;
+    private GameObject MerdCameraHost;
+    private MerdCameraController MerdCameraController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MerdCameraHost = GameObject.Find("MerdCameraHost");
+        MerdCameraController = MerdCameraHost.GetComponent<MerdCameraController>();
+        cameras = MerdCameraController.Cameras;
+        currentCamera = cameras[0];
+
+        currentFishSystem = MerdCameraController.SelectedFishSystem;
+        hologramCamera = transform.Find("MerdHologramCamera").gameObject;
+        holoZ = (transform.GetComponent<BoxCollider>().size.z) / 2;
+        holoY = (transform.GetComponent<BoxCollider>().size.y) / 2;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // getting position between centre and radius (boundary) of active camera
+        Vector3 position = currentCamera.transform.localPosition;
+        float tempZ = position.z * holoZ;
+        float tempY = (position.y + 1) * holoY;     // +1 on y-axis because merd cam's neutral position is (0, -1, 0)
+        hologramCamera.transform.localPosition = new Vector3(0.0f, tempY / currentFishSystem.height, tempZ / currentFishSystem.radius);
+
+        //hologramCamera.transform.localPosition = position;
+        hologramCamera.transform.localRotation = currentCamera.transform.localRotation;
+    }
+
+    public void SetCameraAndFishSystem(Camera camera)
+    {
+        currentCamera = camera;
+        currentFishSystem = MerdCameraController.SelectedFishSystem;
+        Debug.Log("Camera switched");
+        Debug.Log(currentFishSystem.radius);
+    }
+}
