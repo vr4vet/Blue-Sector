8bc5a9620ae79867eb2f6f6ff1852001977906c7
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-06-16 16:10:15 +0200

    feat: added sound instructions to the signboards
    Added audio files for three signs. Created AudioAoeTrigger-prefab and
    other scripts to turn on and off audio based on a box collider.
    
    Refs: #135

---
+++ Assets/FishFeeding/Components/Tutorial/Scripts/PlayAudio.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private bool triggered;
+    public bool TurnOnTutorialAudio { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TurnOnTutorialAudio && triggered && !audioSource.isPlaying) 
+        {
+            audioSource.Play();
+            Invoke(nameof(ResetTrigger), audioSource.clip.length);
+            Debug.Log(audioSource.clip.length);
+        }
+    }
+
+/*    private void PlayAudioSource() 
+    {
+        audioSource.Play();
+    }*/
+
+    public void ResetTrigger() 
+    {
+        triggered = false;
+    }
+
+    public void Trigger() 
+    {
+        triggered = true;
+    }
+}
