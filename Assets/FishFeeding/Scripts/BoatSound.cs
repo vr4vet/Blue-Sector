09f2b9e4b83eeb036951dccdf6f9bbc42842bad3
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-06-29 16:34:21 +0200

    feat; added motorboat driving sounds when tranisitioning scene
    Added motorboat driving sound which plays after stepping on the motor
    boat. Still needs to add this to FishWelfare.
    
    Refs: #163

--- Assets/FishFeeding/Scripts/BoatSound.cs
+++ Assets/FishFeeding/Scripts/BoatSound.cs
@@ -1,13 +1,20 @@
+using BNG;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoatSound : MonoBehaviour
 {
+    //[SerializeField]
+    private PlayAudio playAudio;
+    //[SerializeField]
+    private SceneLoader sceneLoader;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playAudio = GetComponent<PlayAudio>();
+        sceneLoader = GetComponent<SceneLoader>();
     }
 
     // Update is called once per frame
@@ -15,4 +22,16 @@ public class BoatSound : MonoBehaviour
     {
         
     }
+
+    public void PlayAudioThenTransition()
+    {
+        GetComponent<AudioSource>().Play();
+        Invoke("TransitionScene", 4.0f);
+    }
+
+    private void TransitionScene() 
+    {
+        sceneLoader.LoadScene("FishWelfare");
+    }
+
 }
