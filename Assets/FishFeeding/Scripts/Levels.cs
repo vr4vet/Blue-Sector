68a899926fe10c1e0a72597e22a68d7976785356
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-07-03 00:10:33 +0200

    feat: localization for current level-text, translates based on current level #166

--- Assets/FishFeeding/Scripts/Levels.cs
+++ Assets/FishFeeding/Scripts/Levels.cs
@@ -1,12 +1,18 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
 
 public class Levels : MonoBehaviour
 {
-    /*private GameObject currentLevelObject;*/
+    public LocalizedStringTable stringTable = new LocalizedStringTable { TableReference = "My Text" };
     private Modes modes;
     private TextMeshProUGUI currentLevelText;
     private int currentLevel = 1;
+    private string translatedCurrentLevel;
+    private string translatedBasic;
+    private string translatedAdvanced;
 
     void Start()
     {
@@ -19,8 +25,12 @@ public class Levels : MonoBehaviour
     {
         if (currentLevel != 1)
         {
+            if (!LocalizationSettings.InitializationOperation.IsDone)
+            {
+                return;
+            }
             currentLevel = 1;
-            currentLevelText.text = "Nåværende nivå: GRUNNLEGGENDE";
+            UpdateLevelText(translatedBasic);
         }
     }
 
@@ -28,10 +38,48 @@ public class Levels : MonoBehaviour
     {
         if (currentLevel != 2)
         {
+            //Debug.Log("level2");
+            if (!LocalizationSettings.InitializationOperation.IsDone)
+            {
+                return;
+            }
+            //Debug.Log("become lv2");
             currentLevel = 2;
-            currentLevelText.text = "Nåværende nivå: AVANSERT";
+            UpdateLevelText(translatedAdvanced);
         }
     }
+
+    void OnEnable()
+    {
+        stringTable.TableChanged += LoadStrings;
+    }
+
+    void OnDisable()
+    {
+        stringTable.TableChanged -= LoadStrings;
+    }
+
+    void LoadStrings(StringTable stringTable)
+    {
+        translatedCurrentLevel = stringTable.GetEntry("CurrentLevel").GetLocalizedString();
+        translatedBasic = stringTable.GetEntry("Basic").GetLocalizedString();
+        translatedAdvanced = stringTable.GetEntry("Advanced").GetLocalizedString();
+        /*Debug.Log("currentlevel:" + translatedCurrentLevel);
+        Debug.Log("basic:" + translatedBasic);*/
+        if(currentLevel == 1)
+        {
+            UpdateLevelText(translatedBasic);
+        } else
+        {
+            UpdateLevelText(translatedAdvanced);
+        }
+    }
+
+    private void UpdateLevelText(string translatedLevel)
+    {
+        currentLevelText.text = translatedCurrentLevel + " " + translatedLevel.ToUpper();
+    }
+
 
     private void Levels_OnModeChanged(object sender, Mode e)
     {
