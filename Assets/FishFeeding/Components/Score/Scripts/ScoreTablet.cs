5ba9c3adf3b6b1e189714f7fbf086616bb7c320e
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-10-29 17:47:34 +0100

    feat: completed more of the evaluation script for fishfeeding #178

--- Assets/FishFeeding/Components/Score/Scripts/ScoreTablet.cs
+++ Assets/FishFeeding/Components/Score/Scripts/ScoreTablet.cs
@@ -3,9 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Task;
-using TMPro.Examples;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class ScoreTablet : MonoBehaviour
 {
@@ -55,14 +53,24 @@ public class ScoreTablet : MonoBehaviour
         }*/
 
         scoreCount = new Dictionary<string, float>(){
-            {"steerCameraCorrect", 0}, {"steerCameraCount", 0},
-            {"adjustFeedingCorrect", 0}, {"adjustFeedingCount", 0},
-            {"didNotCheckMerdCount", 0}, {"checkMerdCount", 0} };
+            {"steerCameraCorrect", 0}, {"adjustFeedingCorrect", 0}, {"didNotCheckMerdCount", 0}, {"totalChecks", 0}};
             //{"merd1SelectedTime", 0}, {"merd2SelectedTime", 0}, {"merd3SelectedTime", 0}};
+        
+        foreach (Subtask subtask in fishFeedTask.Subtasks) {
+            subtask.Points = 0;
+        }
 
         foreach (Subtask subTask in fishFeedTask.Subtasks) {
              subTask.SetCompleated(false);
         }
+        fishFeedTask.Compleated(false);
+        /*Debug.Log("Steer camera: " + fishFeedTask.GetSubtask("Steer the camera").Points);
+        Debug.Log("Adjust feeding intensity: " + fishFeedTask.GetSubtask("Adjust feeding intensity").Points);
+        Debug.Log("Switch to another cage: " + fishFeedTask.GetSubtask("Switch to another cage").Points);
+
+        Debug.Log("Accuracy: " + taskHolder.GetSkill("Accuracy").GetArchivedPoints());
+        Debug.Log("Camera adjustment: " + taskHolder.GetSkill("Camera adjustment").GetArchivedPoints());
+        Debug.Log("Overview of multiple cages: " + taskHolder.GetSkill("Overview of multiple cages").GetArchivedPoints());*/
     }
 
     // Update is called once per frame
@@ -70,87 +78,93 @@ public class ScoreTablet : MonoBehaviour
 
     }
 
-    public void StartScoring() {
-        /*score = 0;*/
+    /*public void StartScoring() {
+        *//*score = 0;*//*
         InvokeRepeating(nameof(UpdateScore), 1.0f, 1.0f);
-        /*Debug.Log("Score: " + score);*/
+        *//*Debug.Log("Score: " + score);*//*
     }
 
     public void StopScoring() {
         CancelInvoke("UpdateScore");
-    }
+    }*/
 
     public void GiveFinalTabletScore() {
-        fishFeedTask.GetSubtask("Steer the camera").Points = 0;
-        fishFeedTask.GetSubtask("Adjust feeding intensity").Points = 0;
-        fishFeedTask.GetSubtask("Switch to another cage").Points = 0;
-        Debug.Log("Steer camera: " + fishFeedTask.GetSubtask("Steer the camera").Points);
-        Debug.Log("Adjust feeding intensity: " + fishFeedTask.GetSubtask("Adjust feeding intensity").Points);
-        Debug.Log("Switch to another cage: " + fishFeedTask.GetSubtask("Switch to another cage").Points);
         foreach (KeyValuePair<string, float> ele in scoreCount) {
             Debug.Log(ele.Key + ": " + ele.Value);
         }
+
         float steerCameraScore = 0;
-        if (scoreCount["steerCameraCount"] != 0) {
-            steerCameraScore = (scoreCount["steerCameraCorrect"] / scoreCount["steerCameraCount"]) / 3;
+        float adjustFeedingScore = 0;
+        float checkMerdScore = 0;
+
+        if (scoreCount["totalChecks"] != 0) {
+            steerCameraScore = scoreCount["steerCameraCorrect"] / scoreCount["totalChecks"];
+            adjustFeedingScore = scoreCount["adjustFeedingCorrect"] / scoreCount["totalChecks"];
+            checkMerdScore = (scoreCount["totalChecks"] - scoreCount["didNotCheckMerdCount"]) / scoreCount["totalChecks"];
         }
         Debug.Log("steerScore: " + steerCameraScore);
-        fishFeedTask.GetSubtask("Steer the camera").AddPoints((int)Math.Round(steerCameraScore * 100));
-
-        float adjustFeedingScore = 0;
-        if (scoreCount["adjustFeedingCount"] != 0) {
-            adjustFeedingScore = (scoreCount["adjustFeedingCorrect"] / scoreCount["adjustFeedingCount"]) / 3;
-        }
         Debug.Log("adjustFeedingScorew: " + adjustFeedingScore);
-        fishFeedTask.GetSubtask("Adjust feeding intensity").AddPoints((int)Math.Round(adjustFeedingScore * 100));
-
-        float checkMerdScore = 0;
-        if (scoreCount["checkMerdCount"] != 0) {
-            checkMerdScore = ((scoreCount["checkMerdCount"] - scoreCount["didNotCheckMerdCount"]) / scoreCount["checkMerdCount"]) / 3;
-        }
         Debug.Log("checkMerdScore: " + checkMerdScore);
-        fishFeedTask.GetSubtask("Switch to another cage").AddPoints(33);
 
-        Debug.Log("check" + Math.Round(0f * 100));
+        fishFeedTask.GetSubtask("Steer the camera").AddPoints((int)Math.Round(steerCameraScore * 100));
+        fishFeedTask.GetSubtask("Adjust feeding intensity").AddPoints((int)Math.Round(adjustFeedingScore * 100));
+        fishFeedTask.GetSubtask("Switch to another cage").AddPoints((int)Math.Round(checkMerdScore * 100));
+
+        fishFeedTask.Compleated(true);
+        Debug.Log("task completed: " + fishFeedTask.Compleated());
+
 
         Debug.Log("Steer camera: " + fishFeedTask.GetSubtask("Steer the camera").Points);
         Debug.Log("Adjust feeding intensity: " + fishFeedTask.GetSubtask("Adjust feeding intensity").Points);
         Debug.Log("Switch to another cage: " + fishFeedTask.GetSubtask("Switch to another cage").Points);
+
+        Debug.Log("Accuracy: " + taskHolder.GetSkill("Accuracy").GetArchivedPoints());
+        Debug.Log("Camera adjustment: " + taskHolder.GetSkill("Camera adjustment").GetArchivedPoints());
+        Debug.Log("Overview of multiple cages: " + taskHolder.GetSkill("Overview of multiple cages").GetArchivedPoints());
 
         foreach (string key in scoreCount.Keys.ToList()) {
             scoreCount[key] = 0;
         }
 
+        foreach (Subtask subtask in fishFeedTask.Subtasks) {
+            subtask.Points = 0;
+        }
+
     }
 
     private void UpdateScore(FishSystemScript fishSystemScript) {
-        // Checks each time a state of a fish cage changes
-        // Will then check the position of the camera - either correct or not and saves this value
-        // Is the feeding intensity correct for the situation? - 
-        // Is the merd currently selected?
-        // If it is selected but the feeding intensity is wrong then the merd is not checked, get minus for overview or checking cages
-        if (IsCameraPositionCorrect()) {
+        if (IsCameraPositionCorrect(fishSystemScript)) {
             scoreCount["steerCameraCorrect"] += 1;
         }
-        scoreCount["steerCameraCount"] += 1;
 
-        float feedingIntensityScore = IsFeedingIntensityCorrect(fishSystemScript);
+        float feedingIntensityScore = GiveFeedingIntensityScore(fishSystemScript);
         scoreCount["adjustFeedingCorrect"] += feedingIntensityScore;
-        scoreCount["adjustFeedingCount"] += 1;
 
         if (fishSystemScript != selectedFishSystem && feedingIntensityScore == 0) {
             scoreCount["didNotCheckMerdCount"] += 1;
         }
-        scoreCount["checkMerdCount"] += 1;
 
+        scoreCount["totalChecks"] += 1;
 
     }
 
-    private bool IsCameraPositionCorrect() {
+    private bool IsCameraPositionCorrect(FishSystemScript fishSystemScript) {
+        Camera camera = cameras[fishSystemScript];
+        Vector3 zeroVector = new Vector3(0, 0, 0);
+        Vector3 targetDir = zeroVector - camera.transform.localPosition;
+        float sqrLen = targetDir.sqrMagnitude;
+        //float angle = sqrLen;
+        //Debug.Log("fishsystem" + fishSystemScript.merdNr + " BallAngle: " + Vector3.Angle(camera.transform.forward, sphere.gameObject.transform.localPosition - camera.transform.localPosition));
+        Debug.Log("fishsystem" + fishSystemScript.merdNr + " ZeroVectorAngle: " + Vector3.Angle(camera.transform.forward, targetDir));
+        //Debug.Log("fishsystem"+ fishSystemScript.merdNr + " Angle: " + Vector3.Angle(camera.transform.forward, fishSystemScript.gameObject.transform.localPosition - camera.transform.localPosition));
+        Debug.Log("Distance: " + sqrLen);
+        if ((Vector3.Angle(camera.transform.forward, targetDir) > 90 && sqrLen > 7) || camera.transform.localPosition == zeroVector) {
+            return false;
+        }
         return true;
     }
 
-    private float IsFeedingIntensityCorrect(FishSystemScript fishSystemScript) {
+    private float GiveFeedingIntensityScore(FishSystemScript fishSystemScript) {
         if ((fishSystemScript.state == FishSystemScript.FishState.Full && 
             fishSystemScript.feedingIntensity == FishSystemScript.FeedingIntensity.Low) ||
             (fishSystemScript.state == FishSystemScript.FishState.Hungry &&
@@ -166,12 +180,13 @@ public class ScoreTablet : MonoBehaviour
         }
     }
 
-
-
     private void FishSystemChanged(FishSystemScript fishSystemScript) {
         if (!game.startGame) {
             return;
         }
+
+        UpdateScore(selectedFishSystem);
+
         selectedFishSystem = fishSystemScript;
         Subtask switchSubTask = fishFeedTask.GetSubtask("Switch to another cage");
         switchSubTask.SetCompleated(true);
@@ -181,7 +196,6 @@ public class ScoreTablet : MonoBehaviour
             if (subTask != switchSubTask)
                 subTask.SetCompleated(false);
         }
-
     }
 
     public void JoystickGrabbed() {
