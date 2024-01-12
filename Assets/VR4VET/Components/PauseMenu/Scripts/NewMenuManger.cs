77bbb36dd7f46fc9d60479de178ef7ba8c0ed1cc
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2024-01-12 16:26:46 +0100

    feat: added main menu and fixed pointer issues
    Added main menu from the vr4vet developer branch and fixed pointer
    issues with the main menu. Also fixed rendering layer between tablet ui
    and pointer. Main menu can also be opened with X button on left
    controller.
    
    Refs: #206
    Co-authored-by: eamathie <emilmathie@yahoo.no>

--- Assets/VR4VET/Components/PauseMenu/Scripts/NewMenuManger.cs
+++ Assets/VR4VET/Components/PauseMenu/Scripts/NewMenuManger.cs
@@ -1,9 +1,9 @@
-/* Copyright (C) 2022 IMTEL NTNU - All Rights Reserved
- * Developer: Jorge Garcia
- * Ask your questions by email: jorgeega@ntnu.no
+/* Developer: Jorge Garcia
+ * Ask your questions on github: https://github.com/Jorest
  */
 
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,50 +13,58 @@ using UnityEngine.XR;
 
 public class NewMenuManger : MonoBehaviour
 {
-
-    [SerializeField]public GameObject player;
     [SerializeField] public Material PauseSkyboxMat;
     [SerializeField] public Material SkyboxMat;
     [SerializeField] private LayerMask _menuLayers;  //layers mask to put on top when the game is paused
-    [SerializeField] private InputActionAsset _actionAsset; //we need this to block certain actions
     [SerializeField] private Material _wallsMaterial;
     [SerializeField] private bool _holdToOpen;
-
 
     // Defined in Unity, refers to image used in loading animation.
     [SerializeField] private Image LoadingWheel;
 
     private Camera _cam;
-    [SerializeField] private GameObject _aboutCanvas;
     [SerializeField] private GameObject _menuCanvas;
-    public GameObject stateSaverComponent;
+    [SerializeField] private GameObject _settingsCanvas;
+    [SerializeField] private GameObject _aboutCanvas;
+    [SerializeField] private GameObject _languagesCanvas;
+    [SerializeField] private GameObject _remapCanvas;
+    [SerializeField] private GameObject _audioCanvas;
+    // public GameObject stateSaverComponent;
 
     private GameObject _savedStates;
+    [SerializeField]
     private bool _menuOpen = false;
     private float _holdtime = 1.5f;
 
-
+    private List<GameObject> allMenus = new();
+    [SerializeField]
+    private float canvasesDistance;
     /// <summary>
     /// This Script manages all aspects of the Pause Menu:
     /// Toggle, or Hold to Pause
     /// Change transparency of material while pausing
     /// </summary>
 
-    void Start()
+    private void Start()
     {
-               
         _cam = Camera.main;
 
         Color c = _wallsMaterial.color;
         c.a = 1f;
         _wallsMaterial.color = c;
 
+        allMenus.AddRange(new List<GameObject>() { _menuCanvas, _settingsCanvas, _aboutCanvas, _languagesCanvas, _remapCanvas, _audioCanvas });
+        AdjustCanvasDistances();
+
+        foreach (var item in allMenus)
+        {
+            item.SetActive(false);
+        }
+        _menuCanvas.SetActive(true);
     }
 
-
-    public void ToggleMenu()
+    private void ToggleMenu()
     {
-        Debug.Log("norsk");
         _menuOpen = !_menuOpen;
 
         if (_menuOpen)
@@ -65,17 +73,15 @@ public class NewMenuManger : MonoBehaviour
             ResumeGame();
     }
 
-     void PauseGame()
+    private void PauseGame()
     {
         Color c = _wallsMaterial.color;
-        c.a = 0.7f;
+        c.a = 0.8f;
         _wallsMaterial.color = c;
         Time.timeScale = 0; // pauses time events
         RenderSettings.skybox = PauseSkyboxMat;
         _cam.cullingMask = _menuLayers; //show only the chosen menu layers
         _menuCanvas.SetActive(true);
-     
-
     }
 
     public void ResumeGame()
@@ -84,16 +90,10 @@ public class NewMenuManger : MonoBehaviour
         c.a = 1f;
         _wallsMaterial.color = c;
         Time.timeScale = 1;
-        RenderSettings.skybox = SkyboxMat ;
+        RenderSettings.skybox = SkyboxMat;
         _cam.cullingMask = -1; // -1 = "Everything"
         _menuCanvas.SetActive(false);
-        _aboutCanvas.SetActive(false);
-
-
     }
-
-
-  
 
     public void Restart()
     {
@@ -101,22 +101,6 @@ public class NewMenuManger : MonoBehaviour
         Time.timeScale = 1;
         //back to the first scene
         SceneManager.LoadScene(0);
-
-
-
-    }
-
-    public void OpenAbout()
-    {
-        _aboutCanvas.SetActive(true);
-        _menuCanvas.SetActive(false);
-    }
-
-
-    public void CloseAbout()
-    {
-        _aboutCanvas.SetActive(false);
-        _menuCanvas.SetActive(true);
     }
 
     public void OpenSaves()
@@ -144,67 +128,80 @@ public class NewMenuManger : MonoBehaviour
 
     public void ExitGame()
     {
-        stateSaverComponent.GetComponent<stateSaver>().saveObjects();
+        // stateSaverComponent.GetComponent<stateSaver>().saveObjects();
         Application.Quit();
     }
 
-
-
     public void PressHoldMenu(InputAction.CallbackContext context)
     {
-        if (_holdToOpen)
+        if (_holdToOpen && !_menuOpen)
         {
             if (context.started)
             {
-                StartCoroutine(HoldPause());
+                StartCoroutine(HoldPause(context));
             }
-        } else
+        }
+        else if (context.performed)
         {
             ToggleMenu();
         }
-       
     }
 
     // Loading wheel to open the pause menu
-    public IEnumerator HoldPause()
+    private IEnumerator HoldPause(InputAction.CallbackContext context)
     {
-
         for (float I = 0f; I < _holdtime; I += Time.deltaTime)
         {
-
-            // Get left-handed device and its current state (pressed or not).
-            UnityEngine.XR.InputDevice _rightDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
-            _rightDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out bool triggerValue);
-
-            // Fill LoadingWheel if trigger is pressed
-            if (triggerValue)
+            // Yes, this is a try catch checking if it can log the context variable.
+            // Yes, this is to check if the user releases the button.
+            // Yes, this is extremely stupid.
+            // But, it's the only thing I got to work.
+            try
             {
-                LoadingWheel.fillAmount = I;
+                Debug.Log(context);
             }
+            catch(Exception e)
+            {
+                LoadingWheel.fillAmount = 0f;
+                I = 1.6f;
+                yield break;
+            }
+            
+            // Fill LoadingWheel if trigger is pressed
+            LoadingWheel.fillAmount = I;
+            
 
             // Open menu if it has been continuously pressed.
             if (I >= 1f)
             {
-
                 LoadingWheel.fillAmount = 0f;
-                I = _holdtime+1;
-
-                PauseGame();
-            }
-
-            // If trigger is no longer pressed, reset the LoadingWheel.
-            if ((!triggerValue))
-            {
-
-                LoadingWheel.fillAmount = 0f;
-                I = 1.6f;
+                ToggleMenu();
+                yield break;
             }
             yield return null;
         }
     }
 
 
+    public void SwitchMenuTo(GameObject panelToOpen)
+    {
+        foreach (var item in allMenus)
+        {
+            bool shouldSetActive = (item == panelToOpen);
+            if (item.activeSelf != shouldSetActive)
+            {
+                item.SetActive(shouldSetActive);
+            }
+        }
+    }
 
+    void AdjustCanvasDistances()
+    {
+        CanvasFollow[] canvasFollows = GetComponentsInChildren<CanvasFollow>();
 
-
+        foreach (CanvasFollow canvasFollow in canvasFollows)
+        {
+            canvasFollow.AdjustDistance(canvasesDistance);
+        }
+    }
 }
