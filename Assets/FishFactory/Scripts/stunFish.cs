6b42a78d0d46068f0b9f469cb42b70d599010515
Herman1412 <hermansaetre1412@gmail.com>
2024-03-07 18:12:07 +0100

    feat: update stunFish script
    the stun machine only stun fish who have state as alive, and will change their state to stunned
    
    close: #276

--- Assets/FishFactory/Scripts/stunFish.cs
+++ Assets/FishFactory/Scripts/stunFish.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class stunFish : MonoBehaviour
 {
-    private int nrFishStun = 0;
+    // counter for the number of fish that are stunned
+    public int nrFishStun = 0;
     private void OnTriggerEnter(Collider collisionObject)
     {
         if (collisionObject.tag != "Destroyable") { return; }
-        // Gets the parent's parent
+        // get the parent's parent
         GameObject fish = collisionObject.transform.parent.gameObject.transform.parent.gameObject;
-        if (fish.GetComponent<Animator>().enabled == true) 
+
+        // get fish state and check if fish is alive, if fish is alive it's state is set to stunned
+        FactoryFishState fishState = fish.GetComponent<FactoryFishState>();
+        if (fishState.currentState == FactoryFishState.State.Alive) 
         {
-            fish.GetComponent<Animator>().enabled = false;
+            // commented out as animation is not working correctly and the component has been disabled 
+            // fish.GetComponent<Animator>().enabled = false;
+
+            fishState.currentState = FactoryFishState.State.Stunned;
             nrFishStun++;
         }
     }
