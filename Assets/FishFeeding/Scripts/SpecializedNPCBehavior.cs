6f85423b46687576b4c61acc89a52ba705d3d72e
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2024-04-10 15:39:41 +0200

    fix: fixed height of NPC again #368

--- Assets/FishFeeding/Scripts/SpecializedNPCBehavior.cs
+++ Assets/FishFeeding/Scripts/SpecializedNPCBehavior.cs
@@ -31,7 +31,7 @@ public class SpecializedNPCBehavior : MonoBehaviour
         // Adjusts the size of the NPCs
         foreach (GameObject npc in _npcSpawner._npcInstances)
         {
-            npc.transform.localScale = new Vector3(1.07f, 1.07f, 1.07f);
+            npc.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
             Transform dialogueCanvas = npc.transform.GetChild(1);
             dialogueCanvas.localPosition = new Vector3(dialogueCanvas.localPosition.x, dialogueCanvas.localPosition.y, 0.5f);
             dialogueCanvas.localScale = new Vector3(0.008f, 0.008f, 0.008f);
