8e299cfab3f2db2d8080e97f7ec81aa48ff662ae
Jon <128969752+jonp-h@users.noreply.github.com>
2024-04-11 23:48:26 +0200

    feat: create initial stunner device
    Create fish stunner device prefab
    
    Refs: #373

--- Assets/FishFactory/Scripts/stunFish.cs
+++ Assets/FishFactory/Scripts/stunFish.cs
@@ -6,20 +6,25 @@ public class stunFish : MonoBehaviour
 {
     // counter for the number of fish that are stunned
     public int nrFishStun = 0;
+
     private void OnTriggerEnter(Collider collisionObject)
     {
-        if (collisionObject.tag != "Destroyable") { return; }
+        if (collisionObject.tag != "Destroyable")
+        {
+            return;
+        }
         // get the parent's parent
         GameObject fish = collisionObject.transform.parent.gameObject.transform.parent.gameObject;
 
         // get fish state and check if fish is alive, if fish is alive it's state is set to stunned
         FactoryFishState fishState = fish.GetComponent<FactoryFishState>();
-        if (fishState.currentState == FactoryFishState.State.Alive) 
+        if (fishState.currentState == FactoryFishState.State.Alive)
         {
-            // commented out as animation is not working correctly and the component has been disabled 
+            // commented out as animation is not working correctly and the component has been disabled
             // fish.GetComponent<Animator>().enabled = false;
 
             fishState.currentState = FactoryFishState.State.Stunned;
+            GameManager.instance.PlaySound("correct");
             nrFishStun++;
         }
     }
