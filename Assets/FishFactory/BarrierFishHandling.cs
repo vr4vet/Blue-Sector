90b9aa43c8642be0e9f9a476b04a69334d9e6f0c
Markus Johansen <markusarj@gmail.com>
2024-04-16 17:10:23 +0200

    fix: Barrier angles and collision physics

---
+++ Assets/FishFactory/BarrierFishHandling.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierFishHandling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float slideForce = 10f; // You can adjust this value to get the desired sliding effect
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            Vector3 direction = collision.contacts[0].point - transform.position;
+            direction = -direction.normalized;
+
+            rb.AddForce(direction * slideForce, ForceMode.Impulse);
+        }
+    }
+}
