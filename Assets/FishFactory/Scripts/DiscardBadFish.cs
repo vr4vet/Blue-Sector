ece94133221c82407a2d96a646d9dd03fc69697c
Herman1412 <hermansaetre1412@gmail.com>
2024-04-22 18:10:16 +0200

    feat: add logic for displaying bad fish and alive fish and add feedback to the discarding prefab
    allow for a separate prefab to be used if the fish has state bad quality and toggle on the animator if fish has state alive (comented out). logic is added to the discarding box to give user audio feedback
    
    close: #287 and #411

--- Assets/FishFactory/Scripts/DiscardBadFish.cs
+++ Assets/FishFactory/Scripts/DiscardBadFish.cs
@@ -12,7 +12,11 @@ public class DiscardBadFish : MonoBehaviour
     public int DestroyFishTreshold{ get {return destroyFishTreshold;}}
     private int nrFishDiscarded = 0;
     public int NrFishDiscarded{ get {return nrFishDiscarded;}}
-    
+    private int incorrectlyDiscardedFish = 0;
+    public int IncorrectlyDiscardedFish{ get {return incorrectlyDiscardedFish;}}
+    private int correctlyDiscardedFish = 0;
+    public int CorrectlyDiscardedFish{ get {return correctlyDiscardedFish;}}
+
     // list of fish that are currently stunned
     private List<GameObject> discardedFish = new List<GameObject>();
     private void OnTriggerEnter(Collider collisionObject)
@@ -25,7 +29,22 @@ public class DiscardBadFish : MonoBehaviour
         if (!discardedFish.Contains(fish.gameObject)) 
         {
             discardedFish.Add(fish.gameObject);
-            if (discardedFish.Count >= destroyFishTreshold){
+
+            // Check if the fish should have been discarded or not, and give the user feedback
+            if (fishState.currentState == FactoryFishState.State.BadQuality)
+            {
+                GameManager.Instance.PlaySound("correct");
+                correctlyDiscardedFish++;
+            }
+            else
+            {
+                GameManager.Instance.PlaySound("incorrect");
+                incorrectlyDiscardedFish--;
+            }
+
+            // Delete fish if number of fish in bin is larger than destroyFishTreshold
+            if (discardedFish.Count >= destroyFishTreshold)
+            {
                 GameObject fishDelete = discardedFish[0];
                 discardedFish.RemoveAt(0);
                 Destroy(fishDelete);
