a60ff6d36f56ff631e945099a80f95f6075a5cd1
Jon <128969752+jonp-h@users.noreply.github.com>
2024-04-22 21:19:47 +0200

    feat: create guttedFish sorting in QA station
    Create guttedFish sorting logic and devices in QA station
    
    Refs: #391

--- Assets/FishFactory/Scripts/GutFish.cs
+++ Assets/FishFactory/Scripts/GutFish.cs
@@ -10,18 +10,47 @@ public class GutFish : MonoBehaviour
     [SerializeField]
     private Material guttedFish;
 
+    [SerializeField]
+    private Material badFish;
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Destroyable")
         {
-            // TODO: Change texture to gutted fish
-            collider
+            Renderer fishRenderer = collider
                 .transform.parent.transform.parent.transform.Find("Stereo textured mesh")
-                .GetComponent<Renderer>()
-                .material = guttedFish;
-            // Play sound
-            GetComponent<AudioSource>()
-                .Play();
+                .GetComponent<Renderer>();
+
+            Material[] fishMaterials = fishRenderer.materials;
+
+            // Get the main fish object
+            GameObject fish = collider.transform.parent.gameObject.transform.parent.gameObject;
+
+            // Get fish state and check if fish is alive, if fish is alive it's state is set to stunned
+            FactoryFishState fishState = fish.GetComponent<FactoryFishState>();
+
+            if (fishState.currentState == FactoryFishState.State.GuttingSuccess)
+            {
+                // Set first material to gutted fish
+                fishMaterials[0] = guttedFish;
+                // Set the updated materials
+                fishRenderer.materials = fishMaterials;
+                GetComponent<AudioSource>().Play();
+            }
+            else if (fishState.currentState == FactoryFishState.State.GuttingIncomplete)
+            {
+                //TODO: play different sound
+            }
+            else
+            {
+                // Set first material to gutted fish
+                fishMaterials[0] = guttedFish;
+                // Set second material to badfish
+                fishMaterials[1] = badFish;
+                // Set the updated materials
+                fishRenderer.materials = fishMaterials;
+                GetComponent<AudioSource>().Play();
+            }
 
             // Turn on light
             gutLight.enabled = true;
