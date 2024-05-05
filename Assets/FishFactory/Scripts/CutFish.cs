2f19cb58327373f85f88d5f09232eff6d1255eb7
Jon <128969752+jonp-h@users.noreply.github.com>
2024-05-05 12:08:57 +0200

    refactor: refactor cutfish script in fish factory
    refactor cutfish script in fish factory
    
    Refs: #467

--- Assets/FishFactory/Scripts/CutFish.cs
+++ Assets/FishFactory/Scripts/CutFish.cs
@@ -16,24 +16,32 @@ public class CutFish : MonoBehaviour
         _knifeState = knife.GetComponent<KnifeState>();
     }
 
+    private void OnTriggerEnter(Collider collider)
+    {
+        cutEvent(collider.tag, true);
+    }
+
     /// <summary>
     /// When the knife collides with the fish body, the player makes a wrong cut
     /// </summary>
     /// <param name="collision"> The knife object </param>
     private void OnCollisionEnter(Collision collisionObject)
     {
-        cutEvent(collisionObject.collider.tag, "body");
-    }
-    
-    private void OnTriggerEnter(Collider collider)
-    {
-        cutEvent(collider.tag, "gill");
+        cutEvent(collisionObject.collider.tag, false);
     }
 
-    private void cutEvent (string tag, string fishPart)
+    private void cutEvent(string tag, bool isGills)
     {
-        tag != "Knife" && return;
-        fishPart == "gill" fishState.CutFishGills() : fishState.CutFishBody();
-        knifeState.DecrementDurabilityCount();
+        if (tag != "Knife")
+            return;
+        if (isGills)
+        {
+            _fishState.CutFishGills();
+        }
+        else
+        {
+            _fishState.CutFishBody();
+        }
+        _knifeState.DecrementDurabilityCount();
     }
 }
