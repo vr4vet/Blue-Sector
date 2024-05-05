f20896519b2c2b9a0e9c1d6a8a4cfb306fa41c1f
Jon P. Hallem <128969752+jonp-h@users.noreply.github.com>
2024-05-05 21:16:03 +0200

    fix: fix sceneController in fish factory (#477)
    fix sceneController with correct coordinates and comments
    
    Refs: #467

--- Assets/FishFactory/Scripts/SceneLogic/SceneController.cs
+++ Assets/FishFactory/Scripts/SceneLogic/SceneController.cs
@@ -3,15 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    // ----------------- Editor Fields -----------------
+
     [Header("Scene Controller")]
     [Tooltip("The name of the scene to change to")]
     [SerializeField]
     private string sceneName;
 
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+    // ----------------- Unity Functions -----------------
 
     /// <summary>
     /// When the player collides with the scene controller, change the scene
@@ -24,6 +23,8 @@ public class SceneController : MonoBehaviour
             ChangeScene(sceneName);
         }
     }
+
+    // ----------------- Private Functions -----------------
 
     /// <summary>
     /// Change the scene. If the player is currently in the HSE room the requirements has to be fulfilled first.
@@ -41,7 +42,7 @@ public class SceneController : MonoBehaviour
             return;
         }
 
-        // Set the target location (door) the player should be placed by after the scene change
+        // Set the target location (exit door) the player should be placed by after the scene change
         SetPlayerTargetLocation(scene);
 
         // Adds a delegate to trigger when the scene is loaded
@@ -58,75 +59,77 @@ public class SceneController : MonoBehaviour
     /// <param name="scene">The name of the scene to change to</param>
     private void SetPlayerTargetLocation(string scene)
     {
-        // get scene name and destination scene
+        // Get scene name and destination scene in the format "CurrentSceneNameToDestinationScene"
         string targetSpawnLocation = SceneManager.GetActiveScene().name + "To" + scene;
-
-        // switch (targetSpawnLocation)
-        // {
-        //     case "HSERoomToBleedingStation":
-        //         GameManager.Instance.NewPlayerPosition = new Vector3(
-        //             4.88999987f,
-        //             1.06599998f,
-        //             12.4700003f
-        //         );
-        //         break;
-        //     case "HSERoomToQAStation":
-        //         GameManager.Instance.NewPlayerPosition = new Vector3(
-        //             -10.191f,
-        //             1.06599998f,
-        //             -10.0489998f
-        //         );
-        //         break;
-        //     case "BleedingStationToHSERoom":
-        //         GameManager.Instance.NewPlayerPosition = new Vector3(
-        //             0.829999983f,
-        //             1.00600004f,
-        //             -5.25f
-        //         );
-        //         break;
-        //     case "BleedingStationToQAStation":
-        //         GameManager.Instance.NewPlayerPosition = new Vector3(
-        //             -8.76399994f,
-        //             1.06599998f,
-        //             -20.8099995f
-        //         );
-        //         break;
-        //     case "QAStationToHSERoom":
-        //         GameManager.Instance.NewPlayerPosition = new Vector3(
-        //             -4.0079999f,
-        //             1.06599998f,
-        //             -3.95799994f
-        //         );
-        //         break;
-        //     case "QAStationToBleedingStation":
-        //         GameManager.Instance.NewPlayerPosition = new Vector3(
-        //             -13.7299995f,
-        //             1.06599998f,
-        //             12.8199997f
-        //         );
-        //         break;
-        // }
-
-        // Debug.Log(GameManager.Instance.NewPlayerPosition);
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-        // Debug.Log(SceneManager.sceneLoaded.GetInvocationList());
-
-        if (
-            !GameManager.Instance.HSERoomCompleted
-            && SceneManager.GetActiveScene().name == "HSERoom"
-        )
+        switch (targetSpawnLocation)
         {
-            GameManager.Instance.PlaySound("incorrect");
+            case "HSERoomToBleedingStation":
+                GameManager.Instance.NextScenePlayerPosition = new Vector3(
+                    11.1490002f,
+                    1.03299999f,
+                    -6.76000023f
+                );
+                GameManager.Instance.NextScenePlayerRotation = new Vector3(0f, 270f, 0f);
+                break;
+            case "HSERoomToQAStation":
+                GameManager.Instance.NextScenePlayerPosition = new Vector3(
+                    -10.191f,
+                    1.06599998f,
+                    -10.0489998f
+                );
+                GameManager.Instance.NextScenePlayerRotation = new Vector3(0f, 90f, 0f);
+                break;
+            case "BleedingStationToHSERoom":
+                GameManager.Instance.NextScenePlayerPosition = new Vector3(
+                    0.907000005f,
+                    1.01400006f,
+                    -5.0000000f
+                );
+                GameManager.Instance.NextScenePlayerRotation = new Vector3(0f, 270f, 0f);
+                break;
+            case "BleedingStationToQAStation":
+                GameManager.Instance.NextScenePlayerPosition = new Vector3(
+                    -8.9989996f,
+                    1.06599998f,
+                    -20.7859993f
+                );
+                GameManager.Instance.NextScenePlayerRotation = new Vector3(0f, 0f, 0f);
+                break;
+            case "QAStationToHSERoom":
+                GameManager.Instance.NextScenePlayerPosition = new Vector3(
+                    -4.11399984f,
+                    1.01400006f,
+                    -3.93300009f
+                );
+                GameManager.Instance.NextScenePlayerRotation = new Vector3(0f, 0f, 0f);
+                break;
+            case "QAStationToBleedingStation":
+                GameManager.Instance.NextScenePlayerPosition = new Vector3(
+                    -7.92999983f,
+                    1.03299999f,
+                    -6.76000023f
+                );
+                GameManager.Instance.NextScenePlayerRotation = new Vector3(0f, 90f, 0f);
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Move the player to the correct location after the scene has been loaded
+    /// </summary>
+    /// <param name="scene"> The scene that has been loaded</param>
+    /// <param name="mode"> The mode the scene has been loaded in</param>
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // The player should be placed at the initial spawn point if the new player position is not set. This is to prevent the player from spawning at 0,0,0.
+        if (GameManager.Instance.NextScenePlayerPosition == new Vector3(0, 0, 0))
+        {
+            Debug.LogError("Location was not set correctly. Spawning player at initial location.");
+            SceneManager.sceneLoaded -= OnSceneLoaded;
             return;
         }
 
-        GameManager.Instance.PlaySound("door");
-        SceneManager.LoadScene(scene);
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
+        // Moving the player will not work if the XR Rig prefab has been renamed!
         GameObject player = GameObject.Find("XR Rig Advanced VR4VET");
         player.transform.position = GameManager.Instance.NextScenePlayerPosition;
         player.transform.eulerAngles = GameManager.Instance.NextScenePlayerRotation;
@@ -134,6 +137,9 @@ public class SceneController : MonoBehaviour
         // Reset the new player position
         GameManager.Instance.NextScenePlayerPosition = new Vector3(0, 0, 0);
         GameManager.Instance.NextScenePlayerRotation = new Vector3(0, 0, 0);
+
+        GameManager.Instance.IsTaskOn = false;
+        GameManager.Instance.IsSecondaryTaskOn = false;
 
         // Remove the delegate to prevent memory leaks
         SceneManager.sceneLoaded -= OnSceneLoaded;
