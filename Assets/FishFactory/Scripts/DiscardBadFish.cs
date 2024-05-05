5595e861e80b0377891f87085f90a8afaa6de560
Sigve Johan Hov <siggiboyy@gmail.com>
2024-05-05 13:58:16 +0200

    Update comments
    Updated comments in the code to properly explain what the code does

--- Assets/FishFactory/Scripts/DiscardBadFish.cs
+++ Assets/FishFactory/Scripts/DiscardBadFish.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DiscardBadFish : MonoBehaviour
 {
+    // counter for the number of fish discarded
     [SerializeField]
     [Range(0, 15)]
     private int destroyFishTreshold = 5;
@@ -18,22 +19,26 @@ public class DiscardBadFish : MonoBehaviour
     private int correctlyDiscardedFish = 0;
     public int CorrectlyDiscardedFish => correctlyDiscardedFish;
 
+    // list of fish that have been discarded
     private List<GameObject> discardedFish = new List<GameObject>();
 
     private void OnTriggerEnter(Collider collisionObject)
     {
+        // get the parent's parent
         GameObject fish = collisionObject.transform.parent?.parent?.gameObject;
 
-        if (fish == null || fish.tag != "Fish")
+        if (fish == null || !fish.CompareTag("Fish"))
         {
             return;
         }
 
+        // get the fish state script attached to the fish object to check if the fish is of bad quality
         FactoryFishState fishState = fish.GetComponent<FactoryFishState>();
         if (!discardedFish.Contains(fish))
         {
             discardedFish.Add(fish);
 
+            // check if the fish is of bad quality or if the gutting failed and give user feedback
             if (fishState.CurrentState == FactoryFishState.State.BadQuality ||
                 fishState.CurrentState == FactoryFishState.State.GuttingFailure)
             {
@@ -46,6 +51,7 @@ public class DiscardBadFish : MonoBehaviour
                 incorrectlyDiscardedFish--;
             }
 
+            // destroy the fish if the number of discarded fish exceeds the threshold
             if (discardedFish.Count >= destroyFishTreshold)
             {
                 GameObject fishDelete = discardedFish[0];
