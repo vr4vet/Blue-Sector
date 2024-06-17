caf571f6af6590249cf4873bef077e367816ef58
frequin <frederik.friquin@gmail.com>
2024-06-17 10:33:23 +0200

    Finished unit test for KnifeState.cs

--- Assets/FishFactory/Tests/PlayMode/KnifeStateTest.cs
+++ Assets/FishFactory/Tests/PlayMode/KnifeStateTest.cs
@@ -3,23 +3,72 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
 
 public class KnifeStateTest
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void KnifeStateTestSimplePasses()
+    private GameObject Knife;
+    private KnifeState _knifeState;
+    private GameObject Fish;
+    private FactoryFishState _fishState;
+    private GameObject GameManager;
+    private GameObject AudioManager;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
     {
-        // Use the Assert class to test conditions
+        SceneManager.CreateScene("KnifeStateTest");
+        var KnifePrefab = Resources.Load<GameObject>("Prefabs/Knife/FishKnife");
+        Knife = Object.Instantiate(KnifePrefab);
+        _knifeState = Knife.GetComponent<KnifeState>();
+        _knifeState._durabilityCount = 1;
+        var fishPrefab = Resources.Load<GameObject>("Prefabs/Fish/FishFactoryBadFish");
+        Fish = Object.Instantiate(fishPrefab);
+        _fishState = Fish.GetComponent<FactoryFishState>();
+
+    }
+    [Test]
+    public void GetKnifeStateTest() {
+        var knife = GameObject.FindGameObjectsWithTag("Knife");
+        if (knife.Length >=1 && knife[0])
+            _knifeState = knife[0].GetComponent<KnifeState>();
+        Assert.AreEqual(_knifeState.gameObject, Knife,"Knife not found");
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator KnifeStateTestWithEnumeratorPasses()
+    [Test]
+    public void DecrementDurabilityTest()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        int[] Tests = new int[3];
+        Tests[0] = Random.Range(2,int.MaxValue);
+        Tests[1] = Random.Range(0,1);
+        Tests[2] = Random.Range(-1,-int.MaxValue);
+        foreach (int i in Tests){
+            _knifeState._durabilityCount = i;
+            var knifeDurability = Knife.GetComponent<KnifeState>()._durabilityCount;
+            _knifeState.DecrementDurabilityCount();
+            if (knifeDurability == 1 || knifeDurability == 0)
+                Assert.AreEqual(Knife.GetComponent<KnifeState>()._durabilityCount,-1);
+            else if (knifeDurability > 1)
+                Assert.AreEqual(Knife.GetComponent<KnifeState>()._durabilityCount, knifeDurability -1);
+            else 
+                Assert.AreEqual(Knife.GetComponent<KnifeState>()._durabilityCount, knifeDurability);
+        }
+    }
+
+    [Test]
+    public void RandomizeDurabilityTest()
+    {
+        _knifeState._durabilityCount = _knifeState.RandomizeDurability();
+        Assert.IsTrue(5 < _knifeState._durabilityCount, "Randomized integer is too low");
+        Assert.IsTrue(17 > _knifeState._durabilityCount, "Randomized integer is too high");
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown() 
+    {
+        Object.Destroy(Knife);
+        Object.Destroy(Fish);
+        SceneManager.UnloadSceneAsync("KnifeStateTest");
     }
 }
