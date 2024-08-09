f4caacbf1df7de7e3e7a79d5b7fc158ab516d8f0
Emil Aron Andresen Mathiesen <emilmathie@yahoo.no>
2024-08-09 13:16:40 +0200

    feat: made functional zoom and scroll
    - Currently using keyboard to control.
    - Uses a placeholder monitor (black Unity cube) for the time being.
    
    Refs: #579

---
+++ Assets/Laboratory/Components/BeaconApp Microscope/Scripts/MicroscopeMonitor.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicroscopeMonitor : MonoBehaviour
+{
+    [SerializeField] private float ScrollSpeed = 0.01f;
+    [SerializeField] private List<int> MagnificationLevels = new List<int> { 1, 2, 5, 8 };
+    private RawImage RawImage;
+    private Vector2 CurrentXY = new Vector2(0.5f, 0.5f);
+
+    private int CurrentMagnificationStep = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RawImage = GetComponentInChildren<RawImage>();
+    }
+
+    private void Update()
+    {
+        // Zooming
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            CurrentMagnificationStep = (CurrentMagnificationStep + 1) % MagnificationLevels.Count;
+            float Magnification = 1.0f / MagnificationLevels[CurrentMagnificationStep];
+
+            // UV x and y must be offset to keep looking at the same point of image when zooming
+            if (CurrentMagnificationStep == 0)
+            {
+                CurrentXY = new Vector2(0.5f, 0.5f);
+                RawImage.uvRect = new Rect(0f, 0f, Magnification, Magnification);
+            }
+            else
+                RawImage.uvRect = new Rect(CurrentXY.x - (Magnification * 0.5f), CurrentXY.y - (Magnification * 0.5f), Magnification, Magnification);
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Scrolling
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            CurrentXY.x += ScrollSpeed;
+            RawImage.uvRect = new Rect(RawImage.uvRect.x + ScrollSpeed, RawImage.uvRect.y, RawImage.uvRect.width, RawImage.uvRect.height);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            CurrentXY.x -= ScrollSpeed;
+            RawImage.uvRect = new Rect(RawImage.uvRect.x - ScrollSpeed, RawImage.uvRect.y, RawImage.uvRect.width, RawImage.uvRect.height);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            CurrentXY.y += ScrollSpeed;
+            RawImage.uvRect = new Rect(RawImage.uvRect.x, RawImage.uvRect.y + ScrollSpeed, RawImage.uvRect.width, RawImage.uvRect.height);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            CurrentXY.y -= ScrollSpeed;
+            RawImage.uvRect = new Rect(RawImage.uvRect.x, RawImage.uvRect.y - ScrollSpeed, RawImage.uvRect.width, RawImage.uvRect.height);
+        }
+    }
+}
