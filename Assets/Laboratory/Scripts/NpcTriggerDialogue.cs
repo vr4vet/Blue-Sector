63a260314eb7d0637c8059d6d135b142d8fed6ed
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2024-09-24 15:36:53 +0200

    feat: add feature to to reset objects in the fish lab
    add feature which will make the objects in the fish lab reset their position when done with the task
    
    Refs: #603

--- Assets/Laboratory/Scripts/NpcTriggerDialogue.cs
+++ Assets/Laboratory/Scripts/NpcTriggerDialogue.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using InteractiveCalculator;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Serialization;
@@ -16,19 +17,27 @@ public class NpcTriggerDialogue : MonoBehaviour
    public DialogueTree larsDialogueTree;
    [SerializeField]  
    public string npcName;
+   public GameObject basket;
+   public GameObject fish;
+   public GameObject scale;
+   public GameObject calculator;
+   public GameObject numPad;
+   public GameObject handheldCounter;
+   public GameObject slider;
 
    private DialogueBoxController dialogueBoxController;
 
    private void Start()
    {
        dialogueBoxController = FindObjectOfType<DialogueBoxController>();
+       // fish.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
    }  
    
    private void Update()
    {
        if (dialogueBoxController.dialogueTreeRestart != null)
        {
-           if (dialogueBoxController.dialogueTreeRestart.name == "NpcFeedback" && dialogueBoxController._dialogueText.text == dialogueBoxController.dialogueTreeRestart.sections[6].dialogue[0])
+           if (dialogueBoxController.dialogueTreeRestart.name == "LarsDialogue" && dialogueBoxController._dialogueText.text == dialogueBoxController.dialogueTreeRestart.sections[0].dialogue[1])
            {
                ChangeToLars();
            
@@ -70,6 +79,29 @@ public class NpcTriggerDialogue : MonoBehaviour
    public void ChangeToLars()
    {
        dialogueBoxController.StartDialogue(larsDialogueTree, 1, npcName);
+       
+       basket.transform.position = new Vector3(-3.2309f, 0.874f, -1.8569f);
+       basket.transform.rotation = Quaternion.Euler(270f, 358.180023f, 0.0f);
+         
+       // fish.transform.position = new Vector3(-3.2342f, 0.895799994f, -2.30819988f);
+       
+       scale.GetComponent<Scale>().totalWeight = 0;
+       
+       calculator.GetComponent<Calculator>().OnPressedClearAll();
+       
+       numPad.GetComponent<ResultLogger>().SwitchToCondition();
+       numPad.GetComponent<ResultLogger>().SwitchToLength();
+       numPad.GetComponent<ResultLogger>().SwitchToWeight();
+       numPad.GetComponent<ResultLogger>().weight.SetText("Weight");
+       numPad.GetComponent<ResultLogger>().length.SetText("Length");
+       numPad.GetComponent<ResultLogger>().conditionFactor.SetText("Condition Factor");
+       
+       handheldCounter.transform.position = new Vector3(-3.19779992f, 0.883899987f, 1.10399997f);
+       handheldCounter.transform.rotation = Quaternion.Euler(270f, 76.1088028f, 0.0f);
+       
+       slider.transform.position = new Vector3(-3.21790004f, 0.862619996f, 1.67770004f);
+       slider.GetComponent<MicroscopeSlide>().RemoveMicroscopeSlide();
+
    }
    
 }
