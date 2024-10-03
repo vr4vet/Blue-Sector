1229c7011f745d46ab5385bcce6f42bb3ae4a93b
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2024-10-03 14:04:14 +0200

    fix/feat: fixed npc, added teleport to laboratory, baked
    Fixed receptionist npc so she makes sound. Adjusted terrain. There are
    now links to all other scenes from reception scene. Baked with high
    settings
    
    Refs: #605

--- Assets/Reception/Scripts/SpecializedBehaviourReceptionist.cs
+++ Assets/Reception/Scripts/SpecializedBehaviourReceptionist.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SpecializedBehaviourReceptionist : MonoBehaviour
 {
+    private NPCSpawner _npcSpawner;
+    private GameObject _receptionistNpc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        _npcSpawner = GetComponent<NPCSpawner>();
+        if (_npcSpawner == null)
+        {
+            Debug.LogError("No NPCSpawner found");
+            return;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _receptionistNpc = _npcSpawner._npcInstances[0];
+
+        // Moves the dialogue canvas to the receptionist higher
+        Transform dialogueCanvas = _receptionistNpc.transform.GetChild(1);
+        dialogueCanvas.localPosition = new Vector3(dialogueCanvas.localPosition.x, 1.68f, dialogueCanvas.localPosition.z);
     }
 }
