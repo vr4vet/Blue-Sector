7c62235e98d1baa899d833424a6ccc54ad5296f3
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2024-11-11 14:10:00 +0100

    fix: delete uneccesary imports
    delete uneccesary imports from pointincontroller.cs, which caused build errors

--- Assets/VR4VET/Components/NPC/Scripts/AnimationControllers/PointingController.cs
+++ Assets/VR4VET/Components/NPC/Scripts/AnimationControllers/PointingController.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.Tutorials.Core.Editor;
 using UnityEngine;
 
 public class PointingController : MonoBehaviour
