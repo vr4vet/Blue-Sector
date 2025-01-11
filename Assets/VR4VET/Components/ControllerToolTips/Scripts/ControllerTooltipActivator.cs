2936acb45d2cf8d6ad8afd4626d1fb2dba3c2362
Emil Aron Andresen Mathiesen <emilmathie@yahoo.no>
2025-01-11 16:34:05 +0100

    feat: made labels configure themselves
    - automatically appears if button function is not "None".
    - activator script has dropdown menu for all buttons.
    - used to select button function which will be displayed automaticaly.
    - adjusted positioning of all labels.
    - code is convoluted, but the "system" is easy to use.
    
    Refs: #738

--- Assets/VR4VET/Components/ControllerToolTips/Scripts/ControllerTooltipActivator.cs
+++ Assets/VR4VET/Components/ControllerToolTips/Scripts/ControllerTooltipActivator.cs
@@ -1,11 +1,30 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ControllerTooltipActivator : MonoBehaviour
 {
+    [Tooltip("The trigger area will be this many times larger than the bounds of the object")]
     [SerializeField] private float TriggerSizeFactor = 1.5f;
+
     private ControllerTooltipManager _controllerTooltipManager;
+
+    [Header("Left controller buttons")]
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions OculusLeft;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions ThumbstickLeft;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions X;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions Y;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions TriggerFrontLeft;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions TriggerGripLeft;
+
+    [Header("Left controller buttons")]
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions OculusRight;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions ThumbstickRight;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions A;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions B;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions TriggerFrontRight;
+    [SerializeField] private ControllerTooltipManager.ButtonFunctions TriggerGripRight;
+
+    private List<ControllerTooltipManager.ButtonMapping> _buttonMappings;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +34,25 @@ public class ControllerTooltipActivator : MonoBehaviour
         collider.isTrigger = true;
         collider.size = transform.parent.GetComponent<MeshFilter>().mesh.bounds.size * TriggerSizeFactor;
         collider.center = transform.parent.GetComponent<MeshFilter>().mesh.bounds.center;
+
+        // setting up list of buttons and their individual function to send to ControllerTooltipManager.cs
+        _buttonMappings = new List<ControllerTooltipManager.ButtonMapping>
+        {
+            new(ControllerTooltipManager.ControllerButtons.OculusLeft, OculusLeft),
+            new(ControllerTooltipManager.ControllerButtons.ThumbstickLeft, ThumbstickLeft),
+            new(ControllerTooltipManager.ControllerButtons.X, X),
+            new(ControllerTooltipManager.ControllerButtons.Y, Y),
+            new(ControllerTooltipManager.ControllerButtons.TriggerFrontLeft, TriggerFrontLeft),
+            new(ControllerTooltipManager.ControllerButtons.TriggerGripLeft, TriggerGripLeft),
+            new(ControllerTooltipManager.ControllerButtons.OculusRight, OculusRight),
+            new(ControllerTooltipManager.ControllerButtons.ThumbstickRight, ThumbstickRight),
+            new(ControllerTooltipManager.ControllerButtons.A, A),
+            new(ControllerTooltipManager.ControllerButtons.B, B),
+            new(ControllerTooltipManager.ControllerButtons.TriggerFrontRight, TriggerFrontRight),
+            new(ControllerTooltipManager.ControllerButtons.TriggerGripRight, TriggerGripRight)
+        };
+
+
     }
 
 
@@ -27,9 +65,9 @@ public class ControllerTooltipActivator : MonoBehaviour
                 Debug.Log("Left hand entered");
             else if (other.transform.parent.name.Equals("RightController"))
                 Debug.Log("Right hand entered");
-        
+
             if (!_controllerTooltipManager.OculusModelsActive())
-                _controllerTooltipManager.SetOculusHandModels();
+                _controllerTooltipManager.SetOculusHandModels(_buttonMappings);
         }
 
     }
--- Assets/VR4VET/Components/ControllerToolTips/Scripts/ControllerTooltipManager.cs
+++ Assets/VR4VET/Components/ControllerToolTips/Scripts/ControllerTooltipManager.cs
@@ -1,11 +1,9 @@
 using BNG;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Antlr3.Runtime;
 using Unity.VRTemplate;
 using UnityEngine;
+
 
 public class ControllerTooltipManager : MonoBehaviour
 {
@@ -22,14 +20,34 @@ public class ControllerTooltipManager : MonoBehaviour
         public Transform oculus, thumbstick, a, b, trigger_front, trigger_grip;
     }
 
+    public enum ControllerButtons
+    {
+        OculusLeft, ThumbstickLeft, X, Y, TriggerFrontLeft, TriggerGripLeft, OculusRight, ThumbstickRight, A, B, TriggerFrontRight, TriggerGripRight
+    }
+    public enum ButtonFunctions
+    {
+        None, Grab, Click, Start, Open
+    }
+
+    // struct used create a list of mapped buttons in ControllerTooltipActivator.cs
+    public struct ButtonMapping
+    {
+        public ControllerButtons button;
+        public ButtonFunctions function;
+
+        // constructor
+        public ButtonMapping(ControllerButtons btn, ButtonFunctions func)
+        {
+            button = btn;
+            function = func;
+        }
+
+    }
+
     private ControllerButtonsLeft _controllerButtonsLeft = new();
     private ControllerButtonsRight _controllerButtonsRight = new();
     private Transform _controllerModelLeft, _controllerModelRight;
-
     private int _questControllerModelIndex = 0;
-
-    private Vector3 _averageLeftButtonPos = Vector3.zero;
-    private Vector3 _averageRightButtonPos = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -38,16 +56,53 @@ public class ControllerTooltipManager : MonoBehaviour
     }
 
     private bool _oculusModelsActive = false;
-    public void SetOculusHandModels()
+    public void SetOculusHandModels(List<ControllerTooltipManager.ButtonMapping> buttonMappings)
     {
-        _player.transform.GetChild(0).GetComponent<HandModelSelector>().ChangeHandsModel(_questControllerModelIndex);
-        _oculusModelsActive = true;
+        if (!_oculusModelsActive)
+        {
+            _player.transform.GetChild(0).GetComponent<HandModelSelector>().ChangeHandsModel(_questControllerModelIndex);
+            _oculusModelsActive = true;
+
+            foreach (ButtonMapping mapping in buttonMappings)
+            {
+                //Debug.Log(mapping.button + " " + mapping.function);
+                Transform button = mapping.button switch
+                {
+                    ControllerButtons.OculusLeft => _controllerButtonsLeft.oculus,
+                    ControllerButtons.ThumbstickLeft => _controllerButtonsLeft.thumbstick,
+                    ControllerButtons.X => _controllerButtonsLeft.x,
+                    ControllerButtons.Y => _controllerButtonsLeft.y,
+                    ControllerButtons.TriggerFrontLeft => _controllerButtonsLeft.trigger_front,
+                    ControllerButtons.TriggerGripLeft => _controllerButtonsLeft.trigger_grip,
+                    ControllerButtons.OculusRight => _controllerButtonsRight.oculus,
+                    ControllerButtons.ThumbstickRight => _controllerButtonsRight.thumbstick,
+                    ControllerButtons.A => _controllerButtonsRight.a,
+                    ControllerButtons.B => _controllerButtonsRight.b,
+                    ControllerButtons.TriggerFrontRight => _controllerButtonsRight.trigger_front,
+                    ControllerButtons.TriggerGripRight => _controllerButtonsRight.trigger_grip,
+                    _ => null,
+                };
+
+                if (button != null)
+                {
+                    if (mapping.function == ButtonFunctions.None)
+                        button.GetChild(0).gameObject.SetActive(false);
+                    else
+                        button.GetComponentInChildren<TMP_Text>().text = mapping.function.ToString();
+                }
+                else
+                    Debug.LogError("Could not map all buttons");
+            }
+        }
     }
 
     public void SetDefaultHandModels()
     {
-        _player.transform.GetChild(0).GetComponent<HandModelSelector>().ChangeHandsModel(0);
-        _oculusModelsActive = false;
+        if (_oculusModelsActive)
+        {
+            _player.transform.GetChild(0).GetComponent<HandModelSelector>().ChangeHandsModel(0);
+            _oculusModelsActive = false;
+        }
     }
 
     public bool OculusModelsActive()
@@ -72,22 +127,9 @@ public class ControllerTooltipManager : MonoBehaviour
         }
 
         // finding controller model right
-        i = 0;
-        foreach (Transform model in _player.transform.GetComponentInChildren<HandModelSelector>().RightHandGFXHolder)
-        {
-            if (model.name.Equals("ControllerReferences_Right"))
-            {
-                _controllerModelRight = model;
-
-                // making sure both models have the same index/position in hierarchy
-                if (_questControllerModelIndex != i)
-                    Debug.LogError("Oculus models do not have the same index/position in hierarhcy");
-            }
-            i++;
-        }
-
-        _averageLeftButtonPos = CalculateAverageButtonPosition(false);
-        _averageRightButtonPos = CalculateAverageButtonPosition(true);
+        _controllerModelRight = _player.transform.GetComponentInChildren<HandModelSelector>().RightHandGFXHolder.GetChild(_questControllerModelIndex);
+        if (!_controllerModelRight.name.Equals("ControllerReferences_Right"))
+            Debug.LogError("Oculus models' position in hierarhcy differ between hands");
 
         // finding and setting up left controller buttons
         foreach (Transform button in _controllerModelLeft.Find("OculusTouchForQuest2_Left/left_oculus_controller_world"))
@@ -96,27 +138,27 @@ public class ControllerTooltipManager : MonoBehaviour
             {
                 case "b_button_oculus":
                     _controllerButtonsLeft.oculus = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, false));
+                    SetUpToolTip(button, button.localPosition + new Vector3(-.05f, .015f, -.03f));
                     break;
                 case "b_button_x":
                     _controllerButtonsLeft.x = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, false));
+                    SetUpToolTip(button, button.localPosition + new Vector3(-.08f, .03f, 0f));
                     break;
                 case "b_button_y":
                     _controllerButtonsLeft.y = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, false));
+                    SetUpToolTip(button, button.localPosition + new Vector3(.04f, .05f, .01f));
                     break;
                 case "b_thumbstick":
                     _controllerButtonsLeft.thumbstick = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, false));
+                    SetUpToolTip(button, button.localPosition + new Vector3(.03f, .1f, .05f));
                     break;
                 case "b_trigger_front":
                     _controllerButtonsLeft.trigger_front = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, false));
+                    SetUpToolTip(button, button.localPosition + new Vector3(.08f, -.01f, .02f));
                     break;
                 case "b_trigger_grip":
                     _controllerButtonsLeft.trigger_grip = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, false));
+                    SetUpToolTip(button, button.localPosition + new Vector3(.08f, .03f, -.02f));
                     break;
                 default:
                     Debug.LogError("Could not find all Oculus controller buttons");
@@ -130,28 +172,28 @@ public class ControllerTooltipManager : MonoBehaviour
             switch (button.name)
             {
                 case "b_button_oculus":
-                    _controllerButtonsRight.b = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, true));
+                    _controllerButtonsRight.oculus = button;
+                    SetUpToolTip(button, button.localPosition + new Vector3(.05f, .015f, -.03f));
                     break;
                 case "b_button_a":
                     _controllerButtonsRight.a = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, true));
+                    SetUpToolTip(button, button.localPosition + new Vector3(.08f, .03f, 0f));
                     break;
                 case "b_button_b":
                     _controllerButtonsRight.b = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, true));
+                    SetUpToolTip(button, button.localPosition + new Vector3(-.04f, .05f, .01f));
                     break;
                 case "b_thumbstick":
                     _controllerButtonsRight.thumbstick = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, true));
+                    SetUpToolTip(button, button.localPosition + new Vector3(-.03f, .1f, .05f));
                     break;
                 case "b_trigger_front":
                     _controllerButtonsRight.trigger_front = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, true));
+                    SetUpToolTip(button, button.localPosition + new Vector3(-.08f, -.01f, .02f));
                     break;
                 case "b_trigger_grip":
                     _controllerButtonsRight.trigger_grip = button;
-                    SetUpToolTip(button, CalculateFinalTooltipPosition(button, true));
+                    SetUpToolTip(button, button.localPosition + new Vector3(-.08f, .03f, -.02f));
                     break;
                 default:
                     Debug.LogError("Could not find all Oculus controller buttons");
@@ -161,57 +203,60 @@ public class ControllerTooltipManager : MonoBehaviour
     }
 
     private void SetUpToolTip(Transform button, Vector3 toolTipPos)
-    {
-        BezierCurve curve = button.AddComponent<BezierCurve>();
-        LineRenderer line = button.AddComponent<LineRenderer>();
-
-        line.material = LineMaterial;
-        line.startWidth = .004f;
-        line.endWidth = .004f;
-        line.startColor = new Color32(0x00, 0x8C, 0xFA, 0xFF);
-        curve.SetPoints(button, _player.transform.GetChild(0).transform);
-        curve.SetCurveFactor(.5f, .5f);
-        curve.SetSegmentCount(32);
-
-        //GameObject toolTip = GameObject.Instantiate(ToolTipPrefab, )
-        
-    }
-
-    /// <summary>
-    /// This calculates the average position between all the buttons of a given controller.
-    /// It assists in the process of placing the tooltips relative to each button.
-    /// It is assumed that a controller has 6 buttons.
-    /// The average position of all buttons on the right controller is returned if "right" is true.
-    /// Otherwise the average position of buttons on the left controller is returned.
-    /// </summary>
-    /// <param name="right"></param>
-    /// <returns></returns>
-    private Vector3 CalculateAverageButtonPosition(bool right)
-    {
-        string path;        
-        if (!right)
-            path = "OculusTouchForQuest2_Left/left_oculus_controller_world";
-        else
-            path = "OculusTouchForQuest2_Right/right_oculus_controller_world";
-
-        Vector3 average = Vector3.zero;
-        foreach (Transform button in !right ? _controllerModelLeft.Find(path) : _controllerModelRight.Find(path))
-        {
-            average += button.localPosition;
-        }
-        average /= 6;
-
-        return average;
-    }
-
-    private Vector3 CalculateFinalTooltipPosition(Transform button, bool right)
     {        
-        float distX = (!right ? _averageLeftButtonPos.x : _averageRightButtonPos.x) - button.localPosition.x;
-        float distY = (!right ? _averageLeftButtonPos.y : _averageRightButtonPos.y) - button.localPosition.y;
-        float distZ = (!right ? _averageLeftButtonPos.z : _averageRightButtonPos.z) - button.localPosition.z;
+        GameObject toolTip = GameObject.Instantiate(ToolTipPrefab, Vector3.zero, button.rotation);
+        toolTip.transform.SetParent(button.transform);
+        toolTip.transform.localScale /= 500;
+        toolTip.transform.localPosition = toolTipPos;
 
-        Debug.Log(button.name + " " + new Vector3(distX, distY, distZ));
-        return new Vector3(distX, distY, distZ);
+        LineRenderer line = toolTip.AddComponent<LineRenderer>();
+        line.material = LineMaterial;
+        line.startWidth = .003f;
+        line.endWidth = .003f;
+        line.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        line.receiveShadows = false;
+        
+        GradientColorKey[] colorKeys = new GradientColorKey[2];
+        colorKeys[0].color = new Color32(0x00, 0x8C, 0xFA, 0xFF);
+        colorKeys[0].time = 0f;
+        colorKeys[1].color = new Color32(0x00, 0x8C, 0xFA, 0xFF);
+        colorKeys[1].time = 1f;
+
+        GradientAlphaKey[] alphaKeys = new GradientAlphaKey[4];
+        alphaKeys[0].alpha = 0f;
+        alphaKeys[0].time = .075f;
+        alphaKeys[1].alpha = 1f;
+        alphaKeys[1].time = .18f;
+        alphaKeys[2].alpha = 1f;
+        alphaKeys[2].time = .96f;
+        alphaKeys[3].alpha = 0f;
+        alphaKeys[3].time = 1f;
+
+        Gradient colorGradient = new();
+        colorGradient.SetKeys(colorKeys, alphaKeys);
+        line.colorGradient = colorGradient;
+
+        BezierCurve curve = toolTip.AddComponent<BezierCurve>();
+        curve.SetPoints(button, CalculateTooltipAnchor(button, toolTip.transform));
+        curve.SetCurveFactor(.1f, .1f);
+        curve.SetSegmentCount(8);   
+    }
+
+
+    private Transform CalculateTooltipAnchor(Transform button, Transform tooltip)
+    {        
+        float distX = tooltip.localPosition.x;
+        float distY = tooltip.localPosition.y;
+
+        Transform anchor;
+
+        float maxDist = Mathf.Max(Mathf.Abs(distX), Mathf.Abs(distY));
+        if (maxDist == Mathf.Abs(distX))
+            anchor = distX >= 0 ? tooltip.Find("LeftAnchor") : tooltip.Find("RightAnchor");
+        else
+            anchor = distY >= 0 ? tooltip.Find("BottomAnchor") : tooltip.Find("TopAnchor");
+
+        return anchor;
     }
 
 }
