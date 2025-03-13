6a273c286c43026db1ce34c044b8153646b939de
Emil Aron Andresen Mathiesen <emilmathie@yahoo.no>
2025-03-13 14:11:44 +0100

    fix: pixelated text and oculus controller scale
    - Fixed an issue where the "start game" canvas was pixelated.
    - Fixed the scale of the oculus controller model over canvas.
    - Simplified StartGameToolTip.cs
    
    Refs: #816

--- Assets/FishFeeding/Scripts/StartGameToolTip.cs
+++ Assets/FishFeeding/Scripts/StartGameToolTip.cs
@@ -5,26 +5,15 @@ public class StartGameToolTip : MonoBehaviour
     [SerializeField] private GameObject _controller;
     [SerializeField] private GameObject _arrow;
     [SerializeField] private Game _gameScript;
-    [SerializeField] private GameObject canvas;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _gameScript = FindObjectOfType<Game>();
-        canvas = transform.GetChild(0).gameObject;
-        canvas.GetComponent<Canvas>().enabled = false;
-        _controller.SetActive(false);
-        _arrow.SetActive(false);
-    }
+    [SerializeField] private GameObject _canvas;
 
     // Update is called once per frame
     void Update()
     {
-        var canvasComponent = canvas.GetComponent<Canvas>();
         bool visible = _gameScript.CanStartGame && !_gameScript.startGame;
-        if (canvasComponent.enabled != visible)
+        if (_canvas.activeSelf != visible)
         {
-            canvasComponent.enabled = visible;
+            _canvas.SetActive(visible);
             _controller.SetActive(visible);
             _arrow.SetActive(visible);
         }
