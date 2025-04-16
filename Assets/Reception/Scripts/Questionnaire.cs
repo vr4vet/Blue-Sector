cb56917fe7b6fd970fb7a2ebbc4b9e30b638a50d
Selin Yuki Oezkan <selinyo@stud.ntnu.no>
2025-04-16 16:47:44 +0200

    refactor(unity): Change some field names
    Change field in UploadDataDTO.cs to work with Questionnaire.cs. Add method in ActionManager.cs that modifies the "user_information" field.

--- Assets/Reception/Scripts/Questionnaire.cs
+++ Assets/Reception/Scripts/Questionnaire.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class Questionnaire : MonoBehaviour
 {
-    private Dictionary<string, string> _answers = new();
+    private Dictionary<string, string> _userInfo;
     private NPCSpawner _npcSpawner;
     private GameObject _receptionistNpc;
+    private ActionManager _actionManager;
 
     /// <summary>
-    /// Receptionist Rachel will ask the user questions about themselves.
+    /// NPC Receptionist Rachel will ask the user questions about themselves.
     /// The information will be sent to ActionManager, and the LLM in Chat-Service will make use of it.
     /// </summary>
     void Start()
@@ -24,6 +25,8 @@ public class Questionnaire : MonoBehaviour
         
         // Find NPC "Receptionist Rachel"
         _receptionistNpc = _npcSpawner._npcInstances[0];
+        _userInfo = new Dictionary<string, string>();
+        _actionManager = ActionManager.Instance;
         ButtonSpawner.OnAnswer += Questionnaire_OnAnswer;
     }
 
@@ -37,22 +40,23 @@ public class Questionnaire : MonoBehaviour
         // Only listen to when the buttons belonging to Receptionist Rachel's dialogue tree are pressed
         if (name==_receptionistNpc.name)
         {
-            if (_answers.ContainsKey(question))
+            if (_userInfo.ContainsKey(question))
             {
-                _answers[question] = answer;
+                _userInfo[question] = answer;
                 Debug.Log($"Answer to question {question} changed: {answer}");
             }
+
+            else if (question == "Are you satisfied with your answers?" && answer == "Yes")
+            {
+                _actionManager.SetUserInfo(_userInfo);
+                Debug.Log("Sending answers to ActionManager");
+            }
+
             else
             {
-                _answers.Add(question, answer);
+                _userInfo.Add(question, answer);
                 Debug.Log($"{answer}, {question}, {name}");
 
-                // Listen to the end of the questionnaire
-                if (question == "Are you satisfied with your answers?" && answer == "Yes")
-                {
-
-                    Debug.Log("Sending answers to ActionManager");
-                }
             }
                  
         }
