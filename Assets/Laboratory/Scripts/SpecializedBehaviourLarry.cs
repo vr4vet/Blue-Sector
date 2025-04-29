f5922f74df92da8c4e295e95c1ced829901d8a71
Halvor Heien Førde <halvohf@stud.ntnu.no>
2025-04-29 08:25:29 +0200

    feat: add specialized behaviour script to larry npc that reduces dialogue trigger distance, to avoid TTSSpeaker missing error

---
+++ Assets/Laboratory/Scripts/SpecializedBehaviourLarry.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class SpecializedBehaviourLarry : MonoBehaviour
+{
+    [SerializeField]
+    public float triggerDistance = 2.5f; // Distance to trigger the dialogue
+
+    private NPCSpawner _npcSpawner;
+    private GameObject _larryNpc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _npcSpawner = GetComponent<NPCSpawner>();
+        if (_npcSpawner == null)
+        {
+            Debug.LogError("No NPCSpawner found");
+            return;
+        }
+
+        _larryNpc = _npcSpawner._npcInstances[0];
+
+        // Reduce dialogue trigger radius to avoid TTSSpeaker component missing error from spawning inside NPC dialogue trigger distance
+        CapsuleCollider capsuleCollider = _larryNpc.GetNamedChild("CollisionTriggerHandler").GetComponent<CapsuleCollider>();
+        capsuleCollider.radius = triggerDistance;
+    }
+}
