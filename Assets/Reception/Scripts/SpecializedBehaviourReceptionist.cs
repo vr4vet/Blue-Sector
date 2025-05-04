b15c5661b42d718018a71fee1071f59c4dc42904
peter-olai <peterolai.joh@gmail.com>
2025-05-04 15:56:22 +0200

    chore: Updated various AI stuffs
    Aina now spawns next to the pillar (R.I.P - MONSTERA PLANT 202X - 2025)
    Aina's dialouge is

--- Assets/Reception/Scripts/SpecializedBehaviourReceptionist.cs
+++ Assets/Reception/Scripts/SpecializedBehaviourReceptionist.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Attached on the NPCSpawner prefab in the reception scene.
+/// </summary>
+
+// This should probably be changed such that you can choose an npc instead of it just being the indexed 0 one
+// That way it can be set and adjusted for any npc in the scene, would be nice to have.
 public class SpecializedBehaviourReceptionist : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Height adjustment for the dialouge canvas")]
+    private float dialogueCanvasHeightAdjustment = 1.42f;
+
     private NPCSpawner _npcSpawner;
     private GameObject _receptionistNpc;
 
@@ -19,8 +29,12 @@ public class SpecializedBehaviourReceptionist : MonoBehaviour
 
         _receptionistNpc = _npcSpawner._npcInstances[0];
 
-        // Moves the dialogue canvas to the receptionist higher
+        // -- Dialogue canvas -- \\
         Transform dialogueCanvas = _receptionistNpc.transform.GetChild(1);
-        dialogueCanvas.localPosition = new Vector3(dialogueCanvas.localPosition.x, 1.68f, dialogueCanvas.localPosition.z);
+        dialogueCanvas.localPosition = new Vector3(dialogueCanvas.localPosition.x, dialogueCanvasHeightAdjustment, dialogueCanvas.localPosition.z);
+
+        // -- Name tag canvas -- \\
+        Transform nameTagCanvas = _receptionistNpc.transform.GetChild(2);
+        nameTagCanvas.localPosition = new Vector3(nameTagCanvas.localPosition.x, dialogueCanvasHeightAdjustment -0.5f, nameTagCanvas.localPosition.z);
     }
 }
