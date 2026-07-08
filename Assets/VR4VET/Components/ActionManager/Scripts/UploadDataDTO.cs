6fa227457633341533849295acfbeb795dfe22b9
Frequin <frederik.friquin@gmail.com>
2026-07-08 14:15:02 +0200

    Removed access key

--- Assets/VR4VET/Components/ActionManager/Scripts/UploadDataDTO.cs
+++ Assets/VR4VET/Components/ActionManager/Scripts/UploadDataDTO.cs
@@ -16,7 +16,7 @@ namespace UploadDTO
     {
         public string agent_id = "6a21869b09b148f9a74e66f4"; // The agent name of which to communicate
         public string active_role_id; // the agent role to use for current query
-        public string access_key = "TyzYMik8GTBP03SIp3rzOwmTwLnzyvRkJ8vZDxsJoa0="; //RAGdoll agent access key
+        public string access_key = "<YOUR_KEY_HERE>"; //RAGdoll agent access key
         public List<Message> chat_log; // chat log so far to give the LLM context
         public List<string> user_information;
         public List<string> user_actions;
